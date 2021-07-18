4e42776aaa59dd81ec1462861180bc602f1124a0
YuriiSelebynka <selebinkayuriy@gmail.com>
2021-07-18 20:06:15 +0200

    Talend commit

---
+++ src/main/java/edu/yurii/model/Element.java
@@ -0,0 +1,11 @@
+package edu.yurii.model;
+/*
+  @author   Yurii Selebynka
+  @project   course-project
+  @class  Element
+  @version  1.0.0 
+  @since 18.07.2021 - 20.02
+*/
+
+public class Element {
+}
---
+++ src/main/java/edu/yurii/service/element/interfaces/IElementService.java
@@ -0,0 +1,22 @@
+package edu.yurii.service.element.interfaces;
+/*
+  @author   Yurii Selebynka
+  @project   course-project
+  @class  IElementService
+  @version  1.0.0 
+  @since 18.07.2021 - 20.04
+*/
+
+import edu.yurii.model.Element;
+import edu.yurii.model.Item;
+
+import java.util.List;
+
+public interface IElementService {
+
+    Element create(Element element);
+    Element get(String id);
+    Element update(Element element);
+    Element delete(String id);
+    //Element<Element> getAll();
+}
